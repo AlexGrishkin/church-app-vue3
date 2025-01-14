c6f9d759f5bc51c588ecc9c4cc9f661ce61c17a7
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2025-01-14 20:58:30 +0300

    feature(TheNavigationMenu): начал делать меню навигации

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>Main</h1>
+    <Swiper></Swiper>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-<style lang="scss">
+<script setup lang="ts">
+import Swiper from '~/components/common/Swiper.vue';
+</script>
+<style lang="scss" module>
 h1 {
   @include Title;
 
